352cfadbe415b79a9a43f4f4bcb4916adbc40e2e
sass846 <skiran.self@gmail.com>
2023-12-29 19:43:32 +0530

    changed font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -15,8 +15,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "jetbrains:size=16" };
-static const char dmenufont[]       = "jetbrains:size=16";
+static const char *fonts[]          = { "ComicShannsMonoNerdFontMono:size=16" };
+static const char dmenufont[]       = "ComicShannsMonoNerdFontMono:size=16";
 /*  static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
